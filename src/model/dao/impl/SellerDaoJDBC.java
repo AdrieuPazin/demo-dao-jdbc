41b46496c0065121f9752ac15234d399b2a257c5
AdrieuPazin <adrieu.pazin@gmail.com>
2019-12-14 16:27:03 -0300

    mensagem de erro ao inserir registro inexistente

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -107,8 +107,12 @@ public class SellerDaoJDBC implements SellerDao{
 			st = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 			st.setInt(1, id);
 			
-			st.executeUpdate();
+			int rows = st.executeUpdate();
 						
+			if (rows == 0) {
+				throw new DbException("Registro inexistente");
+			}
+			
 		} catch (SQLException e) {
 			
 			throw new DbException(e.getMessage());
