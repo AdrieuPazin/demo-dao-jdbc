740e6ad72facfcd34b16835450339c44743229b4
AdrieuPazin <adrieu.pazin@gmail.com>
2019-12-14 16:17:24 -0300

    Metodo update

--- src/application/Program.java
+++ src/application/Program.java
@@ -26,10 +26,17 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\n INSERT");
-		Seller seller2 = new Seller(null, "Nicolle", "passos@hotmail.com", new Date(), 4000.0, department);
-		sellerDao.insert(seller2);
-		System.out.println("Inserido! Novo ID = " + seller2.getId());
+//		System.out.println("\n INSERT");
+//		Seller seller2 = new Seller(null, "Nicolle", "passos@hotmail.com", new Date(), 4000.0, department);
+//		sellerDao.insert(seller2);
+//		System.out.println("Inserido! Novo ID = " + seller2.getId());
+		
+		
+		System.out.println("\n UPDATE");
+		Seller seller2 = sellerDao.findById(1);
+		seller2.setName("Bob");
+		sellerDao.update(seller2);
+		System.out.println("Atualizado");
 		
 	}
 
